f420eaacc2ba15b4f1130a8b64a3107fd39e9d54
your name <thanh0967669406@gmail.com>
2022-09-11 19:35:48 +0700

    feature create

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("hello world 2")
 	fmt.Println("van thanh")
-	fmt.Println("code thay doi")
+	fmt.Println("code thay doi doi")
 	fmt.Println("code thay doi 3")
-	fmt.Println("code feature update")
+	fmt.Println("code learn update")
+	fmt.Println("create")
 
 }
